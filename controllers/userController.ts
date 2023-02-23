732109193f581886237c3f2108861c066021f3a7
Kashif <l174213@lhr.nu.edu.pk>
2023-02-23 13:23:13 +0500

    First Commit

---
+++ controllers/userController.ts
@@ -0,0 +1,77 @@
+import  { Request, Response, NextFunction, RequestHandler } from 'express';
+
+const { myAsyncHandler } = require('../asyncHandler')
+const User = require('../models/userModel')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+
+// Register a new User
+const registerUser: RequestHandler = myAsyncHandler(async (req : Request, res : Response, next :NextFunction) => {
+    try {
+        let { firstName, lastName, email, password } = req.body
+        let registeredUser  = await User.create({ firstName, lastName, email, password })
+        if (registeredUser){
+            let accessToken: String| undefined =createToken(registeredUser._id.toString(),'mySecretKey', res, next)
+            return res.status(201).json({accessToken,registeredUser})
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+// Verifying to login the Existing User
+const loggedInUser : RequestHandler = myAsyncHandler(async (req :Request, res: Response, next :NextFunction) => {
+    try {
+        if (! await bcrypt.compare(req.body.password, req.body.registeredUser.password)) {
+            return res.status(404).json({ message: 'Invalid Username or Password.' })
+        }
+        let accessToken: String| undefined =createToken(req.body.registeredUser._id.toString(),'mySecretKey', res, next)
+       return res.status(200).json({accessToken, message: req.body.registeredUser.firstName+ req.body.registeredUser.lastName + ' Logged In Successfully' })
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+//Reset Password for the User
+const resetPassword : RequestHandler = myAsyncHandler(async (req :Request, res: Response, next :NextFunction) => {
+    try {
+      let {password } = req.body
+      req.body.registeredUser.password = password
+      await req.body.registeredUser.save()
+      return res.status(200).json({ message:  'Password Changed Successfully', user :  req.body.registeredUser})
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+// Logout the current User
+const loggedOutUser : RequestHandler = (req : Request, res : Response, next : NextFunction) => {
+    try {
+        res.clearCookie("jwt")
+        return res.status(200).json({ message: 'User Logged Out Successfully.' })    
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+// Create a unique token if login or register
+const createToken=(_id : String ,sk : String, res: Response, next : NextFunction)=>{
+    try {
+        let token : String = jwt.sign({_id},sk)
+        res.cookie("jwt",token ,{ expires : new Date (Date.now()+ 600000), httpOnly : true })
+        return token.toString()
+    } catch (error) {
+        next(error)
+    }
+    }
+    
+
+
+module.exports = {
+    registerUser, loggedInUser, resetPassword, loggedOutUser
+}
---
+++ middleware/userMiddleware.ts
@@ -0,0 +1,81 @@
+import {Request, Response, NextFunction, RequestHandler } from "express"
+
+const { myAsyncHandler } = require("../asyncHandler")
+const User = require("../models/userModel")
+const { addUserValidationSchema, loginUserValidationSchema, logoutUserValidationSchema } = require("../validations/userValidationSchema")
+const jwt = require('jsonwebtoken')
+
+
+const validateUser: RequestHandler = myAsyncHandler(async (req :Request, res : Response, next :NextFunction) => {
+    try {
+        if (req.path === '/register') {
+            const value = await addUserValidationSchema.validateAsync(req.body)
+            if (value) {
+                let registeredUser = await User.find({ email: req.body.email })
+                if (registeredUser.length > 0) {
+                    return res.status(404).json({ message: 'This User is already Registered.' })
+                }
+                if (req.body.confirmPassword && req.body.password === req.body.confirmPassword){
+                    req.body.registeredUser = registeredUser[0]
+                    next()
+                }
+                
+            }
+        }
+        else if (req.path === '/logout') {
+            const value = await logoutUserValidationSchema.validateAsync(req.body)
+            if (value) {
+                next()        
+            }
+        }
+        else if (req.path == '/resetPassword'){
+            const value = await addUserValidationSchema.validateAsync(req.body)
+            if (value) {
+                let registeredUser = await User.find({ email: req.body.email, firstName: req.body.firstName, lastName : req.body.lastName })
+                if (registeredUser.length < 0) {
+                    return res.status(404).json({ message: 'Sorry, No such User is found.' })
+                }
+                req.body.registeredUser= registeredUser[0]
+                next()     
+            } 
+        }
+        else if (req.path === '/login') {
+            const value = await loginUserValidationSchema.validateAsync(req.body)
+            if (value) {
+                let registeredUser = await User.find({ email: req.body.email })
+                if (registeredUser.length < 0) {
+                    return res.status(404).json({ message: 'Invalid Username or Password.' })
+                }
+                req.body.registeredUser = registeredUser[0]
+                next()
+            }
+        }
+    } catch (error) {
+        res.status(404)
+        next(error)
+    }
+})
+
+
+const verifyToken : RequestHandler = (req : Request, res :Response, next :NextFunction) => {
+    try {
+        const accessToken = req.cookies.jwt
+        if (accessToken){
+            if (jwt.verify(accessToken, 'mySecretKey')){
+                next()
+            }
+        }
+        else{ 
+            res.status(400).json({message : 'Please Log In to Continue'})
+        }     
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+
+module.exports = {
+    validateUser,
+    verifyToken
+}
